0489b90a68451e755a4c29c420c273bc24496c41
Himanish Kaushal <himanish.kaushal@gmail.com>
2021-02-21 16:29:47 -0500

    add mongodb container and mongoose model

---
+++ models/Employee.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  employeeId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Employee = mongoose.model('Employee', EmployeeSchema);
